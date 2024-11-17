89791cdb6b390cc1522298427109a0e72f9ae34c
jatorrey <jacatorresre@ittepic.edu.mx>
2024-11-17 15:29:32 -0700

    Connection.js

--- src/apis/sendgrid/connection.js
+++ src/apis/sendgrid/connection.js
@@ -1,7 +1,5 @@
 const sgMail = require('@sendgrid/mail');
 
-//API KEY de SendGrid no es posible de hacer push en GitHub, agregar localmente para pruebas.
-
-sgMail.setApiKey('');
+sgMail.setApiKey('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');
 
 module.exports = sgMail;
