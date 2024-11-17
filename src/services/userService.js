64a456969106da47bf2d948af71b040a7ee81457
jatorrey <jacatorresre@ittepic.edu.mx>
2024-11-17 14:23:41 -0700

    Simplificando funciones de update & delete

--- src/services/userService.js
+++ src/services/userService.js
@@ -10,29 +10,15 @@ const userService = {
         return await user.save();
     },
     updateUser: async ({ _id, ...rest }) => {
-        const user = await User.findById(_id);
-        if (!user) throw new Error('User not found');
-
-        if (user.facturapiid){
-            await facturapi.updateUser(user.facturapiid, {
-                legal_name: rest.name || user.name,
-                tax_id: rest.rfc || user.rfc,
-                email: rest.email || user.email,
-                email: rest.email || user.email,
-                address: {
-                    zip: rest.zip || user.zip
-                }
-            });
-        }
-
-        return await User.findByIdAndUpdate(_id, rest, { new: true });
+        const user = await User.findByIdAndUpdate(_id, rest, { new: true });
+        facturapi.updateUser(user.facturapiid, user);
+        return user;
     },
 
     deleteProduct: async (_id) => {
-        const user = await User.findById(_id);
-        if (!user) throw new Error('User not found');
-        if (user.facturapiid){ await facturapi.deleteUser(user.facturapiid); }
-        return await User.findByIdAndDelete(_id);
+        const user = await User.findByIdAndDelete(_id);
+        facturapi.deleteUser(user.facturapiid);
+        return user;
     }
 };
 
