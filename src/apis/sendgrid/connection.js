5381ffd071b2d008cd4c7e3dc0033e7a4a7f5b58
jatorrey <jacatorresre@ittepic.edu.mx>
2024-11-17 15:27:00 -0700

    Modificacion de connection.js

--- src/apis/sendgrid/connection.js
+++ src/apis/sendgrid/connection.js
@@ -1,5 +1,7 @@
 const sgMail = require('@sendgrid/mail');
 
-sgMail.setApiKey('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');
+//API KEY de SendGrid: jQ92-p3JSI6kOmGA5fHOyQ.L1qFOe10-bKTeF1yI_Eb8aWZypIPXCy4LLPPkfJrH5U, agregar el SG. al inicio
+
+sgMail.setApiKey('');
 
 module.exports = sgMail;
