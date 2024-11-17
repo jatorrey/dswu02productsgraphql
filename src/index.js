3eb3dd1bc4e83e8cea5ffc1003c734ccf3857008
jatorrey <jacatorresre@ittepic.edu.mx>
2024-11-17 14:21:36 -0700

    Implementando merge y organizacion de codigo

--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
 // src/index.js
 const { ApolloServer } = require('apollo-server');
 const mongoose = require('mongoose');
+const { mergeTypeDefs, mergeResolvers } = require('@graphql-tools/merge');
+
+//Archivos de producto
 const typeDefsProducts = require('./schemas/productSchema');
-const typeDefsUsers = require('./schemas/userSchema');
 const productResolvers = require('./resolvers/productResolver');
+
+//Archivos de usuario
+const typeDefsUsers = require('./schemas/userSchema');
 const userResolvers = require('./resolvers/userResolver');
+
+//Uniendo archivos
+const typeDefs = mergeTypeDefs([typeDefsProducts, typeDefsUsers]);
+const resolvers = mergeResolvers([productResolvers, userResolvers]);
 
 const startServer = async () => {
   // Conectar a MongoDB
   await mongoose.connect('mongodb+srv://jacatorresre:admin@cluster0.4bnuo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
   
-  const server = new ApolloServer({ typeDefs: [typeDefsProducts, typeDefsUsers], resolvers: [productResolvers, userResolvers] });
+  const server = new ApolloServer({ typeDefs, resolvers });
   
   server.listen().then(({ url }) => {
-    console.log(`Servidor corriendo en ${url}`);
+    console.log(`🚀 Servidor corriendo en ${url}`);
   });
 };
 
