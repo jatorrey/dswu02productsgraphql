9e0c768b3b93c9ba6bd058a3342a3eb5fb66601b
jatorrey <jacatorresre@ittepic.edu.mx>
2024-11-17 14:21:18 -0700

    Cambios en funciones de update & delete

--- src/services/productService.js
+++ src/services/productService.js
@@ -6,29 +6,18 @@ const productService = {
   createProduct: async (args) => {
     const product = new Product(args);
     const facturapiproduct = await facturapi.createProduct(product);
-    // console.log(facturapiproduct);
     product.facturapiid = facturapiproduct.id;
     return await product.save();
   },
   updateProduct: async ({ _id, ...rest }) => {
-    const product = await Product.findById(_id);
-    if (!product) throw new Error('Product not found');
-    
-    if (product.facturapiid){
-      await facturapi.updateProduct(product.facturapiid, {
-        description: rest.description || product.description,
-        price: rest.price || product.price
-      });
-    }
-
-    return await Product.findByIdAndUpdate(_id, rest, { new: true });
-
+    const product = await Product.findByIdAndUpdate(_id, rest, { new: true });
+    facturapi.updateProduct(product.facturapiid, product);
+    return product;
   },
   deleteProduct: async (_id) => {
-    const product = await Product.findById(_id);
-    if (!product) throw new Error('Product not found');
-    if (product.facturapiid){ await facturapi.deleteProduct(product.facturapiid); }
-    return await Product.findByIdAndDelete(_id);
+    const product = await Product.findByIdAndDelete(_id);
+    facturapi.deleteProduct(product.facturapiid);
+    return product;
   }
 };
 
